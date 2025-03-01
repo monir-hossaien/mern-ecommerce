68237006c057e999e167553cece2a8cef9344817
Monir Hossain <monirhdigital@gmail.com>
2025-03-01 11:03:19 +0600

    vercel.json

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,16 +1,18 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy:{
+    proxy: process.env.NODE_ENV === 'development' ? {
       '/api/': {
-        target: "https://monirecommerce.vercel.app"
+        target: "https://monirecommerce.vercel.app", // your backend URL on Vercel
+        changeOrigin: true,
+        secure: false,  // set this to true if using https
+        rewrite: (path) => path.replace(/^\/api/, ''),  // optional: rewrite '/api' to '' if needed
       }
-    }
+    } : {}
   }
-})
+});
