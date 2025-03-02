7dd9fde1eecf634ffcf6f3ded6e87226d92ab196
Monir Hossain <monirhdigital@gmail.com>
2025-03-02 06:11:10 +0600

    vite config.js file updated

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,18 +1,19 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy:{
+    proxy: {
       '/api/': {
-        target: "https://mern-ecommerce-7-xiy6.onrender.com",
+        target: 'https://mern-ecommerce-7-xiy6.onrender.com',  // API endpoint for your backend
         changeOrigin: true,
         secure: false
       }
-    }
+    },
+    port: 4173, // Use Render's assigned PORT or default to 4173
+    host: '0.0.0.0', // Bind to all network interfaces
   }
-})
+});
