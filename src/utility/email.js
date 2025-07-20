56e83a0fa5e3350060e6dd67d09ce31d1bf0d61e
Monir Hossain <monirhdigital@gmail.com>
2025-07-20 12:54:39 +0600

    send email update

--- src/utility/email.js
+++ src/utility/email.js
@@ -1,31 +1,48 @@
 import nodemailer from "nodemailer";
-import {EMAIL_HOST, EMAIL_PASS, EMAIL_PORT, EMAIL_USER} from "../../src/config/config.js";
+import {EMAIL_HOST, EMAIL_PASS, EMAIL_PORT, EMAIL_USER, RESEND_API_KEY} from "../../src/config/config.js";
 
-const SendEmail = async(email, subject, emailBody)=>{
+import { Resend } from 'resend';
+
+// const SendEmail = async(email, subject, emailBody)=>{
 
 
-    let transporter = nodemailer.createTransport({
-        host: EMAIL_HOST,
-        port: EMAIL_PORT,
-        secure: false,
-        auth:{
-            user: EMAIL_USER,
-            pass: EMAIL_PASS
-        },
-        tls:{
-            rejectUnauthorized: false
-        }
-    })
+//     let transporter = nodemailer.createTransport({
+//         host: EMAIL_HOST,
+//         port: EMAIL_PORT,
+//         secure: false,
+//         auth:{
+//             user: EMAIL_USER,
+//             pass: EMAIL_PASS
+//         },
+//         tls:{
+//             rejectUnauthorized: false
+//         }
+//     })
 
-    let mailOptions = {
-        from: "monirhossain0556@gmail.com",
-        to: email,
-        subject,
-        html: emailBody
+//     let mailOptions = {
+//         from: "monirhossain0556@gmail.com",
+//         to: email,
+//         subject,
+//         html: emailBody
+//     }
+
+
+//     return await transporter.sendMail(mailOptions)
+// }
+
+// export default SendEmail;
+const resend = new Resend(RESEND_API_KEY);
+export const SendEmail = async (email, subject, emailBody) => {
+    try {
+        const result = await resend.emails.send({
+            from: 'E-commerce <noreply@ecommerce.onresend.com>', // No need to verify domain
+            to: email,
+            subject,
+            html: emailBody
+        });
+        console.log("Email sent:", result);
+    } catch (err) {
+        console.error("Resend email error:", err);
+        throw err;
     }
-
-
-    return await transporter.sendMail(mailOptions)
-}
-
-export default SendEmail;
+};
